3cee5d190ef4f09d20197faf3dcfbf70fa926582
Simon <simon@vostrolinux.fritz.box>
2019-06-14 15:23:48 +0200

    Cleanup and uncommenting, be aware this program is untestedgit status !

--- ble/ble.h
+++ ble/ble.h
@@ -1,40 +1,40 @@
-//#ifndef BLE_PLANT_MONITOR_H
-//#define BLE_PLANT_MONITOR_H
-//
-//#include <SoftwareSerial.h>
-//#include <string.h>
-//
-//#if (ARDUINO >= 100)
-//#include <Arduino.h>
-//#else
-//#include <WProgram.h>
-//#endif
-//
-//#define STD_RxD (11)
-//#define STD_TxD (10)
-//
-//#define BUFFER_LEN (100)
-//
-//extern char buffer[BUFFER_LEN];
-//
-//#define MASTER (1)    //change this macro to define the Bluetooth as Master or not 
-//
-//#define STD_RCV_TIMEOUT (1000)  // 1 second timeout before cancelling rcv process
-//
-//// extern char rcv_str[100];
-//
-///**
-//   returns the number of characters read and stored into buffer
-//
-//   Be aware though, the software serial buffer can only hold up to 64 bytes.
-//*/
-//int ble_recv(char *buffer, unsigned int buffer_len, unsigned int timeout = STD_RCV_TIMEOUT);
-//
-////int ble_send(char *msg, unsigned int msg_len);
-//int ble_send(char msg[]);
-//
-//void ble_setup(unsigned int pin_RxD = STD_RxD, unsigned int pin_TxD = STD_TxD);
-//
-//bool ble_connected(void);
-//
-//#endif
+#ifndef BLE_PLANT_MONITOR_H
+#define BLE_PLANT_MONITOR_H
+
+#include <SoftwareSerial.h>
+#include <string.h>
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#define STD_RxD (11)
+#define STD_TxD (10)
+
+#define BUFFER_LEN (100)
+
+extern char buffer[BUFFER_LEN];
+
+#define MASTER (1) //change this macro to define the Bluetooth as Master or not
+
+#define STD_RCV_TIMEOUT (1000) // 1 second timeout before cancelling rcv process
+
+// extern char rcv_str[100];
+
+/**
+  returns the number of characters read and stored into buffer
+
+  Be aware though, the software serial buffer can only hold up to 64 bytes.
+*/
+int ble_recv(char *buffer, unsigned int buffer_len, unsigned int timeout = STD_RCV_TIMEOUT);
+
+//int ble_send(char *msg, unsigned int msg_len);
+int ble_send(char msg[]);
+
+void ble_setup(unsigned int pin_RxD = STD_RxD, unsigned int pin_TxD = STD_TxD);
+
+bool ble_connected(void);
+
+#endif
--- ble/ble.ino
+++ ble/ble.ino
@@ -1,246 +1,251 @@
-//#include "ble.h"
-//
-//void setup(void)
-//{
-//  Serial.begin(9600);
-//
-//  ble_setup();
-//
-//  Serial.println("####################");
-//}
-//
-//unsigned int i = 1;
-//
-//void loop(void)
-//{
-//  //ble_send("AT+RESET");
-//
-//  if (ble_connected())
-//  {
-//
-//    Serial.println("###### CONNECTED ######");
-//
-//    char msg[] = "testnachricht";
-//
-//    ble_send(msg);
-//
-//    int msg_len = ble_recv(buffer, BUFFER_LEN); // using default timeout of 1 second
-//
-//    if (msg_len)
-//      Serial.println(buffer);
-//    else
-//      Serial.println("Received nothing.");
-//  }
-//
-//  buffer[0] = '\0';
-//
-//  Serial.print(".");
-//  if (i % 10 == 0)
-//    Serial.print("\n");
-//
-//  i++;
-//  delay(200);
-//}
+#include "ble.h"
 
-
-/*
-  Bluetooth HM13 Demo Code
-  2014 Copyright (c) Seeed Technology Inc.  All right reserved.
-
-  Author: Jacky Zhang
-
-  This demo code is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  For more details about the product please check http://www.seeedstudio.com/depot/
-
-*/
-
-/* Upload this sketch into Arduino Uno and press reset*/
-
-#include <SoftwareSerial.h>   //Software Serial Port
-#define RxD 2
-#define TxD 3
-
-#define MASTER 1    //change this macro to define the Bluetooth as Master or not 
-
-SoftwareSerial blueToothSerial(RxD, TxD); //the software serial port
-
-char recv_str[100];
-
-void setup()
+void setup(void)
 {
-  Serial.begin(9600);   //Serial port for debugging
-  pinMode(RxD, INPUT);    //UART pin for Bluetooth
-  pinMode(TxD, OUTPUT);   //UART pin for Bluetooth
-  Serial.println("\r\nPower on!!");
-  if (setupBlueToothConnection() != 0) while (1); //initialize Bluetooth
+    Serial.begin(9600);
 
-  Serial.println("############################");
+    ble_setup();
+
+    Serial.println("####################");
 }
 
 unsigned int i = 1;
 
-void loop()
+void loop(void)
 {
-  delay(1000);
-  
-  if (blueToothSerial.available())
-  {
-    //in master mode, the bluetooth send message periodically.
-    Serial.println("send: hi");
-    blueToothSerial.print("hi");
-    delay(100);
-    //get any message to print
-    if (recvMsg(1000) == 0)
+    //ble_send("AT+RESET");
+
+    if (ble_connected())
     {
-      Serial.print("recv: ");
-      Serial.print((char *)recv_str);
-      Serial.println("");
+
+        Serial.println("###### CONNECTED ######");
+
+        char msg[] = "testnachricht";
+
+        ble_send(msg);
+
+        int msg_len = ble_recv(buffer, BUFFER_LEN); // using default timeout of 1 second
+
+        if (msg_len)
+            Serial.println(buffer);
+        else
+            Serial.println("Received nothing.");
     }
-  }
 
-  Serial.print(".");
-  if (i % 10 == 0)
-    Serial.println("");
-  i++;
-}
+    buffer[0] = '\0';
 
-//used for compare two string, return 0 if one equals to each other
-int strcmp(char *a, char *b)
-{
-  unsigned int ptr = 0;
-  while (a[ptr] != '\0')
-  {
-    if (a[ptr] != b[ptr]) return -1;
-    ptr++;
-  }
-  return 0;
-}
+    Serial.print(".");
+    if (i % 10 == 0)
+        Serial.print("\n");
 
-//configure the Bluetooth through AT commands
-int setupBlueToothConnection()
-{
-#if MASTER
-  Serial.println("this is MASTER\r\n");
-#else
-  Serial.println("this is SLAVE\r\n");
-#endif
-
-  Serial.print("Setting up Bluetooth link\r\n");
-  delay(3500);//wait for module restart
-
-  //send command to module in different baud rate
-  while (1)
-  {
-    delay(500);
-    blueToothSerial.begin(9600);
-    delay(500);
-    Serial.print("try 9600\r\n");
-    if (sendBlueToothCommand("AT") == 0)
-      break;
-    delay(500);
-    blueToothSerial.begin(115200);
-    delay(500);
-    Serial.print("try 115200\r\n");
-    if (sendBlueToothCommand("AT") == 0)
-      break;
-  }
-
-  //we have to set the baud rate to 9600, since the soft serial is not stable at 115200
-  sendBlueToothCommand("AT+RENEW");//restore factory configurations
-  sendBlueToothCommand("AT+BAUD2");//reset the module's baud rate
-  sendBlueToothCommand("AT+AUTH0");//enable authentication
-  sendBlueToothCommand("AT+RESET");//restart module to take effect
-  blueToothSerial.begin(9600);//reset the Arduino's baud rate
-  delay(3500);//wait for module restart
-  //configure parameters of the module
-  sendBlueToothCommand("AT+VERS?");//get firmware version
-  sendBlueToothCommand("AT+ADDE?");//get EDR MAC
-  sendBlueToothCommand("AT+ADDB?");//get BLE MAC
-  sendBlueToothCommand("AT+NAMEHM-13-EDR");//set EDR name
-  sendBlueToothCommand("AT+NAMBHM-13-BLE");//set BLE name
-  sendBlueToothCommand("AT+PINE123abc");//set EDR password
-  sendBlueToothCommand("AT+PINB123abc");//set BLE password
-  sendBlueToothCommand("AT+SCAN0");//set module visible
-  sendBlueToothCommand("AT+NOTI1");//enable connect notifications
-  sendBlueToothCommand("AT+NOTP1");//enable address notifications
-  sendBlueToothCommand("AT+PIO01");//enable key function
-#if MASTER
-  sendBlueToothCommand("AT+ROLB1");//set to master mode
-#else
-  sendBlueToothCommand("AT+ROLB0");//set to slave mode
-#endif
-
-  //sendBlueToothCommand("AT+RENEW");//restore factory configurations
-  //sendBlueToothCommand("AT+SCAN0");//set module visible
-
-  sendBlueToothCommand("AT+RESET");//restart module to take effect
-  delay(3500);//wait for module restart
-  if (sendBlueToothCommand("AT") != 0) return -1; //detect if the module exists
-  Serial.print("Setup complete\r\n\r\n");
-  return 0;
-}
-
-//send command to Bluetooth and return if there is a response
-int sendBlueToothCommand(char command[])
-{
-  Serial.print("send: ");
-  Serial.print(command);
-  Serial.println("");
-
-  blueToothSerial.print(command);
-  delay(200);
-
-  if (recvMsg(200) != 0) return -1;
-
-  Serial.print("recv: ");
-  Serial.print(recv_str);
-  Serial.println("");
-  return 0;
-}
-
-//receive message from Bluetooth with time out
-int recvMsg(unsigned int timeout)
-{
-  //wait for feedback
-  unsigned int time = 0;
-  unsigned char num;
-  unsigned char i;
-
-  //waiting for the first character with time out
-  i = 0;
-  while (1)
-  {
-    delay(50);
-    if (blueToothSerial.available())
-    {
-      recv_str[i] = char(blueToothSerial.read());
-      i++;
-      break;
-    }
-    time++;
-    if (time > (timeout / 50)) return -1;
-  }
-
-  //read other characters from uart buffer to string
-  while (blueToothSerial.available() && (i < 100))
-  {
-    recv_str[i] = char(blueToothSerial.read());
     i++;
-  }
-  recv_str[i] = '\0';
-
-  return 0;
+    delay(200);
 }
+
+// /*
+//   Bluetooth HM13 Demo Code
+//   2014 Copyright (c) Seeed Technology Inc.  All right reserved.
+
+//   Author: Jacky Zhang
+
+//   This demo code is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU Lesser General Public
+//   License as published by the Free Software Foundation; either
+//   version 2.1 of the License, or (at your option) any later version.
+
+//   This library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   Lesser General Public License for more details.
+
+//   You should have received a copy of the GNU Lesser General Public
+//   License along with this library; if not, write to the Free Software
+//   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+//   For more details about the product please check http://www.seeedstudio.com/depot/
+
+// */
+
+// /* Upload this sketch into Arduino Uno and press reset*/
+
+// #include <SoftwareSerial.h> //Software Serial Port
+// #define RxD 2
+// #define TxD 3
+
+// #define MASTER 1 //change this macro to define the Bluetooth as Master or not
+
+// SoftwareSerial blueToothSerial(RxD, TxD); //the software serial port
+
+// char recv_str[100];
+
+// void setup()
+// {
+//     Serial.begin(9600);   //Serial port for debugging
+//     pinMode(RxD, INPUT);  //UART pin for Bluetooth
+//     pinMode(TxD, OUTPUT); //UART pin for Bluetooth
+//     Serial.println("\r\nPower on!!");
+//     if (setupBlueToothConnection() != 0)
+//         while (1)
+//             ; //initialize Bluetooth
+
+//     Serial.println("############################");
+// }
+
+// unsigned int i = 1;
+
+// void loop()
+// {
+//     delay(1000);
+
+//     if (blueToothSerial.available())
+//     {
+//         //in master mode, the bluetooth send message periodically.
+//         Serial.println("send: hi");
+//         blueToothSerial.print("hi");
+//         delay(100);
+//         //get any message to print
+//         if (recvMsg(1000) == 0)
+//         {
+//             Serial.print("recv: ");
+//             Serial.print((char *)recv_str);
+//             Serial.println("");
+//         }
+//     }
+
+//     Serial.print(".");
+//     if (i % 10 == 0)
+//         Serial.println("");
+//     i++;
+// }
+
+// //used for compare two string, return 0 if one equals to each other
+// int strcmp(char *a, char *b)
+// {
+//     unsigned int ptr = 0;
+//     while (a[ptr] != '\0')
+//     {
+//         if (a[ptr] != b[ptr])
+//             return -1;
+//         ptr++;
+//     }
+//     return 0;
+// }
+
+// //configure the Bluetooth through AT commands
+// int setupBlueToothConnection()
+// {
+// #if MASTER
+//     Serial.println("this is MASTER\r\n");
+// #else
+//     Serial.println("this is SLAVE\r\n");
+// #endif
+
+//     Serial.print("Setting up Bluetooth link\r\n");
+//     delay(3500); //wait for module restart
+
+//     //send command to module in different baud rate
+//     while (1)
+//     {
+//         delay(500);
+//         blueToothSerial.begin(9600);
+//         delay(500);
+//         Serial.print("try 9600\r\n");
+//         if (sendBlueToothCommand("AT") == 0)
+//             break;
+//         delay(500);
+//         blueToothSerial.begin(115200);
+//         delay(500);
+//         Serial.print("try 115200\r\n");
+//         if (sendBlueToothCommand("AT") == 0)
+//             break;
+//     }
+
+//     //we have to set the baud rate to 9600, since the soft serial is not stable at 115200
+//     sendBlueToothCommand("AT+RENEW"); //restore factory configurations
+//     sendBlueToothCommand("AT+BAUD2"); //reset the module's baud rate
+//     sendBlueToothCommand("AT+AUTH0"); //enable authentication
+//     sendBlueToothCommand("AT+RESET"); //restart module to take effect
+//     blueToothSerial.begin(9600);      //reset the Arduino's baud rate
+//     delay(3500);                      //wait for module restart
+//     //configure parameters of the module
+//     sendBlueToothCommand("AT+VERS?");         //get firmware version
+//     sendBlueToothCommand("AT+ADDE?");         //get EDR MAC
+//     sendBlueToothCommand("AT+ADDB?");         //get BLE MAC
+//     sendBlueToothCommand("AT+NAMEHM-13-EDR"); //set EDR name
+//     sendBlueToothCommand("AT+NAMBHM-13-BLE"); //set BLE name
+//     sendBlueToothCommand("AT+PINE123abc");    //set EDR password
+//     sendBlueToothCommand("AT+PINB123abc");    //set BLE password
+//     sendBlueToothCommand("AT+SCAN0");         //set module visible
+//     sendBlueToothCommand("AT+NOTI1");         //enable connect notifications
+//     sendBlueToothCommand("AT+NOTP1");         //enable address notifications
+//     sendBlueToothCommand("AT+PIO01");         //enable key function
+// #if MASTER
+//     sendBlueToothCommand("AT+ROLB1"); //set to master mode
+// #else
+//     sendBlueToothCommand("AT+ROLB0"); //set to slave mode
+// #endif
+
+//     //sendBlueToothCommand("AT+RENEW");//restore factory configurations
+//     //sendBlueToothCommand("AT+SCAN0");//set module visible
+
+//     sendBlueToothCommand("AT+RESET"); //restart module to take effect
+//     delay(3500);                      //wait for module restart
+//     if (sendBlueToothCommand("AT") != 0)
+//         return -1; //detect if the module exists
+//     Serial.print("Setup complete\r\n\r\n");
+//     return 0;
+// }
+
+// //send command to Bluetooth and return if there is a response
+// int sendBlueToothCommand(char command[])
+// {
+//     Serial.print("send: ");
+//     Serial.print(command);
+//     Serial.println("");
+
+//     blueToothSerial.print(command);
+//     delay(200);
+
+//     if (recvMsg(200) != 0)
+//         return -1;
+
+//     Serial.print("recv: ");
+//     Serial.print(recv_str);
+//     Serial.println("");
+//     return 0;
+// }
+
+// //receive message from Bluetooth with time out
+// int recvMsg(unsigned int timeout)
+// {
+//     //wait for feedback
+//     unsigned int time = 0;
+//     unsigned char num;
+//     unsigned char i;
+
+//     //waiting for the first character with time out
+//     i = 0;
+//     while (1)
+//     {
+//         delay(50);
+//         if (blueToothSerial.available())
+//         {
+//             recv_str[i] = char(blueToothSerial.read());
+//             i++;
+//             break;
+//         }
+//         time++;
+//         if (time > (timeout / 50))
+//             return -1;
+//     }
+
+//     //read other characters from uart buffer to string
+//     while (blueToothSerial.available() && (i < 100))
+//     {
+//         recv_str[i] = char(blueToothSerial.read());
+//         i++;
+//     }
+//     recv_str[i] = '\0';
+
+//     return 0;
+// }
